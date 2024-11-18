8c7fd204e6c509f694ff7d0d838887c07d4c31bc
Erivaldo <cavalcanteerivaldo0@gmail.com>
2024-11-18 16:16:24 -0300

    my account

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./src/contexts/auth_provider";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "./src/services/query-client";
+import MyAccount from "./src/screens/my_account";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <NavigationContainer>
-          <Routes />
+          <MyAccount />
         </NavigationContainer>
       </AuthProvider>
     </QueryClientProvider>
--- src/components/adress.tsx
+++ src/components/adress.tsx
@@ -7,21 +7,21 @@ export default function Adress() {
       <View>
         <Text style={tw`mb-1`}>CEP da sua casa</Text>
         <TextInput
-          style={tw`flex px-2 py-4 rounded bg-stone-200 w-full`}
+          style={tw`flex px-2 py-4 rounded bg-stone-100 w-full`}
           placeholder="00.000-00"
         />
       </View>
       <View>
         <Text style={tw`mb-1`}>Nome da rua</Text>
         <TextInput
-          style={tw`flex px-2 py-4 rounded bg-stone-200 w-full`}
+          style={tw`flex px-2 py-4 rounded bg-stone-100 w-full`}
           placeholder="Nome da rua"
         />
       </View>
       <View>
         <Text style={tw`mb-1`}>Número</Text>
         <TextInput
-          style={tw`flex px-2 py-4 rounded bg-stone-200 w-full`}
+          style={tw`flex px-2 py-4 rounded bg-stone-100 w-full`}
           placeholder="Número da casa ou apartamento"
         />
       </View>
@@ -29,14 +29,14 @@ export default function Adress() {
         <View style={tw`w-46`}>
           <Text style={tw`mb-1`}>Cidade</Text>
           <TextInput
-            style={tw`fl px-2 py-4 rounded-2 bg-stone-200 w-full`}
+            style={tw`fl px-2 py-4 rounded-2 bg-stone-100 w-full`}
             placeholder="Cidade"
           />
         </View>
         <View style={tw`w-46`}>
           <Text style={tw`mb-1`}>Estado</Text>
           <TextInput
-            style={tw`fl px-2 py-4 rounded-2 bg-stone-200 w-full`}
+            style={tw`fl px-2 py-4 rounded-2 bg-stone-100 w-full`}
             placeholder="Estado"
           />
         </View>
