87aa61fef8ee7b131718ef6b2625c77e1019471c
Toni Moreira <moreiraatoni@gmail.com>
2024-11-20 12:50:26 -0300

    style: remove unused import from app.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./src/contexts/auth_provider";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "./src/services/query-client";
-import MyOrderDelivery from "./src/screens/my_order_delivery";
 import CartProvider from "./src/contexts/cart_provider";
 
 function App() {
