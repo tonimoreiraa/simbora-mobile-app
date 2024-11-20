7a3938a30fdf7439a94160e8c856efa611234766
Erivaldo <cavalcanteerivaldo0@gmail.com>
2024-11-20 04:27:10 -0300

    ui location update

--- src/components/location.tsx
+++ src/components/location.tsx
@@ -5,7 +5,7 @@ import tw from 'twrnc';
 export default function Location() {
   return (
     <View style={tw`flex flex-row items-center py-4`}>
-      <Icon name="location" size={26} />
+      <Icon name="location-sharp" size={26} color='gray' />
       <View style={tw`flex flex-row items-center px-2`}>
         <Text style={tw`text-stone-300`}>Enviar para </Text>
         <TouchableOpacity>
